cf8169cdee86bb1b6c31a2d3e2121adbf8c1d8df
Dust-Dust <446840401@qq.com>
2023-04-24 17:12:54 +0800

    关于我页面的初步静态页面设计

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+
+  goToMyGithub(): void {
+    window.open('https://github.com/Dust-Dust')
+  }
 }
