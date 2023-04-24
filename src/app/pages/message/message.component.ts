63697f72a7ed966fdbed0409a7abd5de9f465d44
Dust-Dust <446840401@qq.com>
2023-04-24 17:01:03 +0800

    拟定留言板的初步静态页面设计

--- src/app/pages/message/message.component.ts
+++ src/app/pages/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { addDays, formatDistance } from 'date-fns';
 
 @Component({
   selector: 'app-message',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+
+  inputValue?: string;
+
+  data = [
+    {
+      author: '再花',
+      content: '这是一条留言',
+      date: '2023年4月24日16:54:43'
+    },
+    {
+      author: '方长',
+      content: '这也是一条留言',
+      date: '2023年4月24日16:55:16'
+    },
+  ];
 
   constructor() { }
 
